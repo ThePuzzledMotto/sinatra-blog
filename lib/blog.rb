b6515e162dba6b2449dced1cb294c59dfda7b60d
ThePuzzledMotto <138157572+ThePuzzledMotto@users.noreply.github.com>
2023-11-13 17:06:21 +0000

    Setting up executable

--- lib/blog.rb
+++ lib/blog.rb
@@ -1,15 +1,19 @@
 require 'sinatra'
 require './blog/posts.rb'
 
-configure do
-    set :port, 8080  
-end
+class App
+    def self.run()
+        configure do
+            set :port, 8080  
+        end
 
-posts = Posts.new
+        posts = Posts.new
 
-posts.insert 'First Post', 'This is the first Post'
-posts.insert 'Second Post', 'This is the second Post'
+        posts.insert 'First Post', 'This is the first Post'
+        posts.insert 'Second Post', 'This is the second Post'
 
-get '/' do
-    erb :index, locals: { posts: posts.get_posts }
+        get '/' do
+            erb :index, locals: { posts: posts.get_posts }
+        end
+    end
 end
